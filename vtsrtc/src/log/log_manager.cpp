97c7dbbed33aa54213687a0ca00ba26455ba812a
dijunkun <dijunkun@zhejianglab.com>
2022-10-31 14:33:33 +0800

    Update vtslog, save log files in /data/log in apollo

--- vtsrtc/src/log/log_manager.cpp
+++ vtsrtc/src/log/log_manager.cpp
@@ -4,12 +4,16 @@ LogManager::LogManager() {
 }
 
 LogManager::~LogManager() {
-	log_.ClsFile( "rtc_agent" );
+	log_.ClsFile("rtc_agent");
 }
 
 int LogManager::init() {
-	log_.InitLog( false, vts::log::LEVEL_INFO );
-	log_.CrtFile( "rtc_agent" );
-	//log_.StartUpLoad( vts::log::TCP, "10.0.104.86:10086", "" );
+#if defined  __aarch64__
+	log_.InitLog(false, vts::log::LEVEL_INFO, "/data/log/");
+#else
+	log_.InitLog(false, vts::log::LEVEL_INFO);
+#endif
+	log_.CrtFile("rtc_agent");
+	//log_.StartUpLoad(vts::log::TCP, "10.0.104.86:10086", "");
 	return 0;
 }
