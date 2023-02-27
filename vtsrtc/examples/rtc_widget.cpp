a342af0f08bdce8be71a15a09cae0ee86e4d50a3
dijunkun <dijunkun@zhejianglab.com>
2023-02-27 15:23:58 +0800

    Fix qt display

--- vtsrtc/examples/rtc_widget.cpp
+++ vtsrtc/examples/rtc_widget.cpp
@@ -240,7 +240,7 @@ void RtcWidget::CreateUI() {
 	videosource_layout->addWidget(videosource_label);
 	videosource_layout->addWidget(videosources_combobox_);
 	videosource_groupbox->setLayout(videosource_layout);
-	videosource_groupbox->setFixedSize(660, 50);
+	videosource_groupbox->setFixedSize(660, 65);
 
 	// 房间管理
 	QGroupBox* room_groupbox = new QGroupBox(tr("Room Management"));
@@ -260,7 +260,7 @@ void RtcWidget::CreateUI() {
 	room_layout->addWidget(join_room_btn, 1, 3, 1, 1);
 	room_layout->addWidget(leave_room_btn, 1, 4, 1, 1);
 	room_groupbox->setLayout(room_layout);
-	room_groupbox->setFixedSize(660, 80);
+	room_groupbox->setFixedSize(660, 100);
 
 	// SRS管理
 	QGroupBox* SRS_groupbox = new QGroupBox(tr("SRS Management"));
@@ -323,7 +323,10 @@ void RtcWidget::CreateUI() {
 	main_layout->addWidget(room_groupbox);
 	main_layout->addWidget(SRS_groupbox);
 	main_layout->addWidget(msg_groupbox);
-#if !defined  __aarch64__
+#if defined  __aarch64__
+	QGroupBox* temp_groupbox = new QGroupBox(tr("Render"));
+	main_layout->addWidget(temp_groupbox);
+#else
 	main_layout->addWidget(Render_groupbox);
 #endif
 	main_layout->addWidget(tableView_);
